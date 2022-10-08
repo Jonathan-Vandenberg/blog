8595f9924362c45897f80fd510d8f3951cfa00f5
jon-vdb <jonvdberg8@gmail.com>
2022-10-08 17:10:41 +0700

    alt logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,82 +13,11 @@ export default function App() {
 
   return (
     <>
-      <header>
-        <nav className="items-center pt-5 px-4 mx-auto max-w-screen-xl sm:px-8 md:flex md:space-x-6">
-          <div className="flex justify-between">
-            <a href="javascript:void(0)">
-              <img
-                src="https://www.floatui.com/logo.svg"
-                width={120}
-                height={50}
-                alt="Float UI logo"
-              />
-            </a>
-            <button
-              className="text-gray-500 outline-none md:hidden"
-              onClick={() => setState(!state)}
-            >
-              {state ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
-          <ul
-            className={`flex-1 justify-between mt-12 md:flex md:mt-0 ${
-              state ? "" : "hidden"
-            }`}
-          >
-            <li className="order-2 pb-5 md:pb-0">
-              <a
-                href="javascript:void(0)"
-                className="py-3 px-6 rounded-md shadow-md text-white text-center bg-indigo-500 focus:shadow-none block md:inline"
-              >
-                Sign In
-              </a>
-            </li>
-            <div className="order-1 flex-1 justify-center items-center space-y-5 md:flex md:space-x-6 md:space-y-0">
-              {navigation.map((item, idx) => (
-                <li className="text-gray-500 hover:text-indigo-600" key={idx}>
-                  <a href={item.path}>{item.title}</a>
-                </li>
-              ))}
-            </div>
-          </ul>
-        </nav>
-      </header>
       <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
         <div className="text-center space-y-4">
           <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
             Optimize your website for
-            <span className="text-indigo-600">Search engine</span>
+            <span className="text-indigo-600"> Search engine</span>
           </h1>
           <p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
             It is a long established fact that a reader will be distracted by
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "../components/Footer";
+import Nav from "../components/Nav";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <div className="fixed top-0 left-0 w-full z-50">
+        <Nav />
+      </div>
       <Component {...pageProps} />
       <Footer />
     </>
